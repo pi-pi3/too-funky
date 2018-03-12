d34e1ffcef1b449a39b79196870cc082cb0c941a
Szymon Walter <walter.szymon.98@gmail.com>
2018-03-12 09:24:56 +0100

    rewrite lidt

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -12,5 +12,5 @@ pub type InterruptHandler = unsafe extern fn(NoCall);
 pub type ExceptionHandler<E> = unsafe extern fn(ErrArgs<E>, NoCall);
 
 pub unsafe fn lidt(idtr: &Idtr) {
-    asm!("lidt [$0]": : "r"(idtr) : "memory" : "intel", "volatile");
+    asm!("lidt $0": : "m"(idtr) : "memory" : "intel", "volatile");
 }
